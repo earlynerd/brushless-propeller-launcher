9917cbe333d9dea2e9a613c5048a1586dc0a8b0f
earlynerd <hyperneurone@gmail.com>
2024-06-10 18:03:20 -0700

    initial commit

---
+++ scripts/pioasm.py
@@ -0,0 +1,38 @@
+Import("env")
+
+"""
+Custom pioasm compiler script for platformio.
+(c) 2022 by Piotr Zapart
+https://gist.github.com/hexeguitar/f4533bc697c956ac1245b6843e2ef438
+"""
+from os.path import join
+import glob
+import sys
+import os
+
+print("RAN PIOASM?")
+
+platform = env.PioPlatform()
+PIO_FILES = []
+for root, dirs, files in os.walk(env["PROJECT_INCLUDE_DIR"]):
+    for file in files:
+        if file.endswith(".pio"):
+            PIO_FILES.append(os.path.join(root, file))
+# print("Looking here:", PIO_FILES)
+
+if PIO_FILES:
+    print("==============================================")
+    print('PIO ASSEMBLY COMPILER')
+    try:
+        PIOASM_DIR = platform.get_package_dir("tool-pioasm-rp2040-earlephilhower")
+    except:
+        print("tool-pioasm-rp2040-earlephilhower not found!")
+        print("please install it using the following command:")
+        print("pio pkg install -g --tool \"earlephilhower/tool-pioasm-rp2040-earlephilhower@^5.100300.220714\"")
+        sys.exit()
+
+    PIOASM_EXE = join(PIOASM_DIR, "pioasm")
+    print("pio files found:")
+    for filename in PIO_FILES:
+        env.Execute(PIOASM_EXE + f' -o c-sdk {filename} {filename}.h')
+    print("==============================================")
---
+++ src/main.cpp
@@ -0,0 +1,109 @@
+#include <Arduino.h>
+#include "dshot/esc.h"
+#include "BlHeli-Passthrough/esc_passthrough.h"
+
+#define triggerPin 9
+#define throttlePin 5
+
+DShot::Type escType = DShot::Type::Normal;
+
+DShot::ESC dshot(throttlePin, pio0, escType, DShot::Speed::DS600, 14);
+
+void setup()
+{
+  Serial.begin(115200);
+  // while(!Serial);
+  delay(500);
+  pinMode(triggerPin, INPUT_PULLUP);
+  pinMode(throttlePin, OUTPUT);
+  pinMode(LED_BUILTIN, OUTPUT);
+  if (!digitalRead(triggerPin)) // battery plugged in with trigger pulled
+  {                             // enter esc passthrough mode
+    beginPassthrough(throttlePin);
+    while (true)
+    {
+      bool breakout = processPassthrough(); // the function returns true when you hit the "disconnect" button in BLHeliSuite32. You can ignore that if you want to be able to reconnect
+      if (breakout)
+        break;
+    }
+    endPassthrough(); // this restores the previous state of the pins, i.e. which peripheral they were assigned to. It will also free the sm and delete the pio program from this passthrough.
+  }
+  if (dshot.init())
+    Serial.println("DSHOT init success!");
+  else
+    Serial.println("DSHOT init fail");
+  delay(500);
+  delay(2);
+  uint32_t startTime = millis();
+  while (millis() - startTime < 1000)
+    dshot.setCommand(13); // 1046 is the example command
+  //startTime = millis();
+  // while (millis() - startTime < 1000)
+  // dshot.setCommand(13); // extended telemetry enable
+  //dshot.setCommand(0);
+}
+
+DShot::Telemetry telemetry = {0};
+bool lastPressed = false;
+uint32_t pressStartTime;
+const double spinUpRampTime = 1.50;
+uint64_t raw_telemetry;
+
+void loop()
+{
+  bool pressed = !digitalRead(triggerPin);
+  if (!pressed)
+  {
+    dshot.setThrottle(0.0);
+    digitalWrite(LED_BUILTIN, LOW);
+    // Serial.println("throttle: 0.000");
+  }
+  else
+  {
+    digitalWrite(LED_BUILTIN, HIGH);
+    if (lastPressed == false)
+    {
+      pressStartTime = millis();
+    }
+    uint32_t pressDuration = millis() - pressStartTime;
+    double duration = pressDuration / 1000.0;
+    if (duration > spinUpRampTime)
+      duration = spinUpRampTime;
+
+    double throttle = duration / spinUpRampTime;
+    dshot.setThrottle(throttle);
+  }
+  lastPressed = pressed;
+  if (escType == DShot::Type::Bidir)
+  {
+    delay(2);
+    // uint64_t raw_telemetry;
+
+    if (dshot.getRawTelemetry(raw_telemetry))
+    {
+      // Serial.print("Tel: ");
+      // print_bin(raw_telemetry);
+      dshot.decodeTelemetry(raw_telemetry, telemetry);
+      if (Serial)
+      {
+        Serial.print("rpm:");
+        Serial.print(telemetry.rpm);
+        Serial.print(", temperature_C:");
+        Serial.print(telemetry.temperature_C);
+        Serial.print(", volts_cV:");
+        Serial.print(telemetry.volts_cV / 100);
+        Serial.print(".");
+        Serial.print(telemetry.volts_cV % 100);
+        Serial.print(", amps_A:");
+        Serial.print(telemetry.amps_A);
+        Serial.print(", errors:");
+        Serial.print(telemetry.errors);
+        Serial.println("");
+      }
+    }
+  }
+  // else
+  // {
+  //  Serial.println("No telemetry :(");
+  //}
+}
